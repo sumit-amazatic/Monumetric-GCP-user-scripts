c358db6ee14f2c7d68cc6ec2079f861cf05274e1
dnyaneshwar-amazatic <dnyaneshwar.m@amazatic.com>
2019-12-24 17:42:26 +0530

    restart script added

---
+++ scripts/restart_staging.sh
@@ -0,0 +1,2 @@
+python3 scaledown.py aggregator-staging; python3 scaledown.py dashboard-staging; python3 scaledown.py services-staging
+python3 scaleup.py aggregator-staging; python3 scaleup.py dashboard-staging; python3 scaleup.py services-staging
